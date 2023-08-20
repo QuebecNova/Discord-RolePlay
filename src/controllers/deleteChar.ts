d54a51b173b2bbb2b387e5cb6ff3ba2e9f38691a
QuebecNova <selllmenya@gmail.com>
2023-08-20 23:38:35 +0700

    fix

--- src/controllers/deleteChar.ts
+++ src/controllers/deleteChar.ts
@@ -6,19 +6,21 @@ import { sendMessage } from '../helpers/sendMessage'
 export const deleteChar = async (req: Request, res: Response) => {
 	const { data } = req.body
 	const name = data.options?.[0]?.value as string
-
+	const user = req.user.doc
 	if (!name) throw new BadRequestError('Pass a name of a character!')
 
-	if (!req.user.doc || !req.user.doc?.chars?.length)
+	if (!user || !user?.chars?.length)
 		throw new BadRequestError("You don't have any characters!")
 
-	const char = req.user.doc.chars.find(
+	const char = user.chars.find(
 		char =>
 			char.name === name && char.user_discord_id === req.user.discord_id
 	)
 
 	if (!char) throw new BadRequestError('Character you specified not found')
 
+	user.chars = user.chars.filter(char => char.id !== char.id)
+	await user.save()
 	await char.deleteOne()
 
 	sendMessage(req, res, {
